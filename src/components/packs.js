2d403e80bd68cedd46094a7b4d1d1ecd40ac5966
ElRharbiBadr <stardust12003@gmail.com>
2023-12-20 14:17:32 +0100

    first commit

---
+++ src/components/packs.js
@@ -0,0 +1,99 @@
+import barca from '../images/barca.png';
+import mad from '../images/mad.png';
+import man from '../images/man.png';
+import bay from '../images/bay.png';
+import paris from '../images/paris.png';
+import arsenal from '../images/arsenal.png';
+import villarreal from '../images/villarreal.png';
+import Silver from '../images/Silver.png';
+import Gold from '../images/Gold.png';
+import Thunder from '../images/Thunder.png';
+import Icon from '../images/Icon.png';
+import GK from '../images/GK.png';
+
+const packs = [
+    {
+        name:"FC Barcelona",
+        price:1500,
+        id:"barcelona",
+        img:barca
+    },
+    {
+        name:"Real Madrid",
+        price:1500,
+        id:"Real Madrid",
+        img:mad
+
+    },
+    {
+        name:"Manchester City F.C.",
+        price:3000,
+        id:"Man city",
+        img:man
+    },
+    {
+        name:"Bayern Munich",
+        price:1500,
+        id:"Bayern Munich",
+        img:bay
+
+    },
+    {
+        name:"Paris Saint-Germain F.C.",
+        price:2000,
+        id:"Paris Saint-Germain",
+        img:paris
+
+    },
+    {
+        name:"Arsenal F.C.",
+        price:1500,
+        id:"Arsenal",
+        img:arsenal
+
+    },
+    {
+        name:"Villarreal CF",
+        price:1000,
+        id:"Villarreal FC",
+        img:villarreal
+
+    },
+    {
+        name:"Random Silver Pack",
+        price:500,
+        id:"S",
+        img:Silver
+
+    },
+    {
+        name:"Random Gold Pack",
+        price:2000,
+        id:"G",
+        img:Gold
+
+    },
+    {
+        name:"Random Thunder Struck Pack",
+        price:5000,
+        id:"T",
+        img:Thunder
+
+    },
+    {
+        name:"Random Icon Pack",
+        price:20000,
+        id:"I",
+        img:Icon
+
+    },
+    {
+        name:"Random Goal Keeper Pack",
+        price:2000,
+        id:"K",
+        img:GK
+
+    },
+]
+
+export default packs;
---
+++ src/components/playersinfo.js
@@ -0,0 +1,580 @@
+import one from '../images/1.png';
+import two from '../images/2.png';
+import three from '../images/3.png';
+import four from '../images/4.png';
+import five from '../images/5.png';
+import six from '../images/6.png';
+import seven from '../images/7.png';
+import eight from '../images/8.png';
+import nine from '../images/9.png';
+import ten from '../images/10.png';
+import eleven from '../images/11.png';
+import twelve from '../images/12.png';
+import thirteen from '../images/13.png';
+import fourteen from '../images/14.png';
+import fifteen from '../images/15.png';
+import sixteen from '../images/16.png';
+import seventeen from '../images/17.png';
+import eighteen from '../images/18.png';
+import nineteen from '../images/19.png';
+import twenty from '../images/20.png';
+import twentyOne from '../images/21.png';
+import twentyTwo from '../images/22.png';
+import twentyThree from '../images/23.png';
+import twentyFour from '../images/24.png';
+import twentyFive from '../images/25.png';
+import twentySix from '../images/26.png';
+import twentySeven from '../images/27.png';
+import twentyEight from '../images/28.png';
+import twentyNine from '../images/29.png';
+import thirty from '../images/30.png';
+import thirtyOne from '../images/31.png';
+import thirtyTwo from '../images/32.png';
+import thirtyThree from '../images/33.png';
+import thirtyFour from '../images/34.png';
+import thirtyFive from '../images/35.png';
+import thirtySix from '../images/36.png';
+import thirtySeven from '../images/37.png';
+import thirtyEight from '../images/38.png';
+import thirtyNine from '../images/39.png';
+import forty from '../images/40.png';
+import fortyOne from '../images/41.png';
+import fortyTwo from '../images/42.png';
+import fortyThree from '../images/43.png';
+import fortyFour from '../images/44.png';
+import fortyFive from '../images/45.png';
+import fortySix from '../images/46.png';
+import fortySeven from '../images/47.png';
+import fortyEight from '../images/48.png';
+import fortyNine from '../images/49.png';
+import fifty from '../images/50.png';
+import fiftyOne from '../images/51.png';
+import fiftyTwo from '../images/52.png';
+import fiftyThree from '../images/53.png';
+import fiftyFour from '../images/54.png';
+import fiftyFive from '../images/55.png';
+import fiftySix from '../images/56.png';
+import fiftySeven from '../images/57.png';
+import fiftyEight from '../images/58.png';
+import fiftyNine from '../images/59.png';
+import sixty from '../images/60.png';
+import sixtyOne from '../images/61.png';
+import sixtyTwo from '../images/62.png';
+import sixtyThree from '../images/63.png';
+import sixtyFour from '../images/64.png';
+import sixtyFive from '../images/65.png';
+import sixtySix from '../images/66.png';
+import sixtySeven from '../images/67.png';
+import sixtyEight from '../images/68.png';
+import sixtyNine from '../images/69.png';
+import seventy from '../images/70.png';
+import seventyOne from '../images/71.png';
+import seventyTwo from '../images/72.png';
+import seventyThree from '../images/73.png';
+import seventyFour from '../images/74.png';
+import seventyFive from '../images/75.png';
+import seventySix from '../images/76.png';
+import seventySeven from '../images/77.png';
+import seventyEight from '../images/78.png';
+import seventyNine from '../images/79.png';
+import eighty from '../images/80.png';
+import eightyOne from '../images/81.png';
+import eightyTwo from '../images/82.png';
+import eightyThree from '../images/83.png';
+import eightyFour from '../images/84.png';
+
+
+const playerinfo = [
+{
+	name: "Robert Lewandowski",
+	team: "barcelona",
+	image:one,
+	rating:91,
+	rarity:"T",
+	score:{
+		pac:77,
+		sho:92,
+		pas:82,
+		dri:88,
+		def:45,
+		phy:85		
+}
+},
+{
+	name: "Ilkay Gündogan",
+	team: "barcelona",
+	image:two,
+	rarity:"T",
+	rating:86,
+	score:{
+		pac:62,
+		sho:81,
+		pas:84,
+		dri:86,
+		def:72,
+		phy:73		
+}
+},
+{
+	name: "Oriol Romeu",
+	team: "barcelona",
+	image:three,
+	rarity:"G",
+	rating:80,
+	score:{
+		pac:43,
+		sho:54,
+		pas:69,
+		dri:67,
+		def:79,
+		phy:82,
+}
+},
+{
+	name: "Andreas Christensen",
+	team: "barcelona",
+	image:four,
+	rarity:"G",
+	rating:83,
+	score:{
+		pac:67,
+		sho:32,
+		pas:68,
+		dri:71,
+		def:85,
+		phy:76,	
+}
+},
+{
+	name: "Sergi Roberto",
+	team: "barcelona",
+	image:five,
+	rarity:"G",
+	rating:80,
+	score:{
+		pac:74,
+		sho:63,
+		pas:79,
+		dri:77,
+		def:75,
+		phy:73,
+}
+},
+{
+	name: "Joao Felix",
+	team: "barcelona",
+	image:six,
+	rarity:"G",
+	rating:81,
+	score:{
+		pac:81,
+		sho:79,
+		pas:78,
+		dri:85,
+		def:40,
+		phy:67,
+}
+},
+{
+	name: "Pau Víctor",
+	team: "barcelona",
+	image:seven,
+	rarity:"B",
+	rating:62,
+	score:{
+		pac:76,
+		sho:58,
+		pas:60,
+		dri:65,
+		def:52,
+		phy:67,
+}
+},
+{
+	name: "Marc Casadó",
+	team: "barcelona",
+	image:eight,
+	rarity:"B",
+	rating:64,
+	score:{
+		pac:60,
+		sho:54,
+		pas:61,
+		dri:66,
+		def:61,
+		phy:53,		
+}
+},
+{
+	name: "Ronald Araujo",
+	team: "barcelona",
+	image:nine,
+	rating:86,
+	rarity:"T",
+	score:{
+		pac:79,
+		sho:51,
+		pas:65,
+		dri:62,
+		def:86,
+		phy:84,
+}
+},
+{
+	name: "Inigo Martinez",
+	team: "barcelona",
+	image:ten,
+	rating:82,
+	rarity:"G",
+	score:{
+		pac:71,
+		sho:56,
+		pas:67,
+		dri:61,
+		def:82,
+		phy:80,		
+}
+},
+{
+	name: "Ronaldinho",
+	team: "barcelona",
+	image:eleven,
+	rarity:"I",
+	rating:93,
+	score:{
+		pac:91,
+		sho:89,
+		pas:90,
+		dri:95,
+		def:38,
+		phy:80,
+}
+},
+{
+	name: "Marc-André Ter Stege",
+	team: "barcelona",
+	image:twelve,
+	rarity:"K",
+	rating:89,
+	score:{
+		div:86,
+		han:85,
+		kic:89,
+		ref:91,
+		spe:47,
+		pos:86,		
+}
+},
+{
+	name: "Vinicius Junior",
+	team: "Real Madrid",
+	image:thirteen,
+	rarity:"T",
+	rating:89,
+	score:{
+		pac:95,
+		sho:82,
+		pas:78,
+		dri:90,
+		def:29,
+		phy:68,
+}
+},
+{
+	name: "Ferland Mendy",
+	team: "Real Madrid",
+	image:fourteen,
+	rarity:"G",
+	rating:85,
+	score:{
+		pac:95,
+		sho:68,
+		pas:80,
+		dri:80,
+		def:80,
+		phy:89,
+}
+},
+{
+	name: "Luka Modric",
+	team: "Real Madrid",
+	image:fifteen,
+	rarity:"T",
+	rating:91,
+	score:{
+		pac:80,
+		sho:81,
+		pas:92,
+		dri:91,
+		def:80,
+		phy:71,
+}
+},
+{
+	name: "Brahim Diaz",
+	team: "Real Madrid",
+	image:sixteen,
+	rarity:"G",
+	rating:80,
+	score:{
+		pac:81,
+		sho:71,
+		pas:77,
+		dri:83,
+		def:30,
+		phy:49,	
+}
+},
+{
+	name: "Arda Guler",
+	team: "Real Madrid",
+	image:seventeen,
+	rarity:"G",
+	rating:77,
+	score:{
+		pac:72,
+		sho:70,
+		pas:80,
+		dri:81,
+		def:52,
+		phy:50,		
+}
+},
+{
+	name: "Alvaro Carrillo",
+	team: "Real Madrid",
+	image:eighteen,
+	rarity:"B",
+	rating:62,
+	score:{
+		pac:68,
+		sho:60,
+		pas:59,
+		dri:59,
+		def:22,
+		phy:56,
+	}
+},
+{
+	name: "Fran García",
+	team: "Real Madrid",
+	image:nineteen,
+	rarity:"G",
+	rating:78,
+	score:{
+		pac:89,
+		sho:49,
+		pas:69,
+		dri:77,
+		def:71,
+		phy:68,	
+}
+},
+{
+	name: "Rafael Obrado",
+	team: "Real Madrid",
+	image:twenty,
+	rarity:"B",
+	rating:65,
+	score:{
+		pac:79,
+		sho:31,
+		pas:48,
+		dri:57,
+		def:61,
+		phy:63,
+}
+},
+{
+	name: "Antonio Rüdiger",
+	team: "Real Madrid",
+	image:twentyOne,
+	rarity:"G",
+	rating:85,
+	score:{
+		pac:82,
+		sho:54,
+		pas:71,
+		dri:66,
+		def:84,
+		phy:86
+	}
+},
+{
+	name: "Jude Bellingham",
+	team: "Real Madrid",
+	image:twentyTwo,
+	rarity:"T",
+	rating:88,
+	score:{
+		pac:79,
+		sho:80,
+		pas:81,
+		dri:87,
+		def:80,
+		phy:84	
+}
+},
+{
+	name: "Ronaldo Luís",
+	team: "Real Madrid",
+	image:twentyThree,
+	rarity:"I",
+	rating:94,
+	score:{
+		pac:94,
+		sho:94,
+		pas:79,
+		dri:94,
+		def:43,
+		phy:75		
+}
+},
+{
+	name: "Thibaut Courtois",
+	team: "Real Madrid",
+	image:twentyFour,
+	rarity:"K",
+	rating:90,
+	score:{
+		div:85,
+		han:89,
+		kic:76,
+		ref:93,
+		spe:46,
+		pos:90		
+}
+},
+{
+	name: "Kyle Walker",
+	team: "Man city",
+	image:twentyFive,
+	rarity:"G",
+	rating:84,
+	score:{
+		pac:90,
+		sho:63,
+		pas:77,
+		dri:78,
+		def:79,
+		phy:81		
+}
+},
+{
+	name: "Kevin De Bruyne",
+	team: "Man city",
+	image:twentySix,
+	rarity:"T",
+	rating:87,
+	score:{
+		pac:80,
+		sho:84,
+		pas:91,
+		dri:83,
+		def:70,
+		phy:74		
+}
+},
+{
+	name: "Ruben Dias",
+	team: "Man city",
+	image:twentySeven,
+	rarity:"T",
+	rating:90,
+	score:{
+		pac:65,
+		sho:40,
+		pas:68,
+		dri:70,
+		def:90,
+		phy:88		
+}
+},
+{
+	name: "Jack Grealish",
+	team: "Man city",
+	image:twentyEight,
+	rarity:"T",
+	rating:88,
+	score:{
+		pac:85,
+		sho:80,
+		pas:87,
+		dri:91,
+		def:56,
+		phy:76		
+}
+},
+{
+	name: "Erling Haaland",
+	team: "Man city",
+	image:twentyNine,
+	rarity:"T",
+	rating:92,
+	score:{
+		pac:91,
+		sho:94,
+		pas:67,
+		dri:82,
+		def:46,
+		phy:90		
+}
+},
+{ name: "Rico Lewis", team: "Man city", image: thirty, rarity: "S", rating: 73, score: { pac: 74, sho: 43, pas: 67, dri: 72, def: 70, phy: 59 } },
+{ name: "Oscar Bobb", team: "Man city", image: thirtyOne, rarity: "B", rating: 64, score: { pac: 80, sho: 59, pas: 59, dri: 68, def: 31, phy: 40 } },
+{ name: "Bernardo Silva", team: "Man city", image: thirtyTwo, rarity: "T", rating: 88, score: { pac: 69, sho: 78, pas: 86, dri: 92, def: 70, phy: 69 } },
+{ name: "Kalvin Phillips", team: "Man city", image: thirtyThree, rarity: "T", rating: 86, score: { pac: 77, sho: 72, pas: 84, dri: 83, def: 86, phy: 82 } },
+{ name: "Benjamin Knight", team: "Man city", image: thirtyFour, rarity: "B", rating: 60, score: { pac: 67, sho: 57, pas: 52, dri: 68, def: 28, phy: 38 } },
+{ name: "Yaya Touré", team: "Man city", image: thirtyFive, rarity: "I", rating: 88, score: { pac: 80, sho: 84, pas: 87, dri: 85, def: 82, phy: 90 } },
+{ name: "Ederson Santana", team: "Man city", image: thirtySix, rarity: "K", rating: 88, score: { div: 86, han: 82, kic: 91, ref: 86, spe: 64, pos: 86 } },
+{ name: "Matthijs de Ligt", team: "Bayern Munich", image: thirtySeven, rarity: "T", rating: 86, score: { pac: 66, sho: 61, pas: 64, dri: 68, def: 85, phy: 86 } },
+{ name: "Noussair Mazraoui", team: "Bayern Munich", image: thirtyEight, rarity: "G", rating: 82, score: { pac: 79, sho: 66, pas: 77, dri: 83, def: 77, phy: 74 } },
+{ name: "Mathys Tel", team: "Bayern Munich", image: thirtyNine, rarity: "S", rating: 72, score: { pac: 76, sho: 73, pas: 62, dri: 75, def: 26, phy: 60 } },
+{ name: "Kingsley Coman", team: "Bayern Munich", image: forty, rarity: "T", rating: 87, score: { pac: 91, sho: 80, pas: 82, dri: 89, def: 32, phy: 65 } },
+{ name: "Tarek Buchmann", team: "Bayern Munich", image: fortyOne, rarity: "B", rating: 60, score: { pac: 57, sho: 29, pas: 40, dri: 41, def: 61, phy: 67 } },
+{ name: "Raphaël Guerreiro", team: "Bayern Munich", image: fortyTwo, rarity: "G", rating: 82, score: { pac: 65, sho: 78, pas: 85, dri: 88, def: 76, phy: 55 } },
+{ name: "Harry Kane", team: "Bayern Munich", image: fortyThree, rarity: "T", rating: 92, score: { pac: 70, sho: 94, pas: 86, dri: 85, def: 50, phy: 84 } },
+{ name: "Thomas Müller", team: "Bayern Munich", image: fortyFour, rarity: "G", rating: 84, score: { pac: 59, sho: 82, pas: 83, dri: 79, def: 55, phy: 69 } },
+{ name: "Konrad Laimer", team: "Bayern Munich", image: fortyFive, rarity: "G", rating: 85, score: { pac: 81, sho: 72, pas: 80, dri: 80, def: 83, phy: 81 } },
+{ name: "Bouna Sarr", team: "Bayern Munich", image: fortySix, rarity: "S", rating: 71, score: { pac: 70, sho: 69, pas: 72, dri: 74, def: 66, phy: 66 } },
+{ name: "Franck Ribéry", team: "Bayern Munich", image: fortySeven, rarity: "I", rating: 88, score: { pac: 89, sho: 85, pas: 85, dri: 90, def: 37, phy: 65 } },
+{ name: "Manuel Neuer", team: "Bayern Munich", image: fortyEight, rarity: "K", rating: 87, score: { div: 84, han: 85, kic: 90, ref: 84, spe: 49, pos: 88 } },
+{ name: "Danilo Pereira", team: "Paris Saint-Germain", image: fortyNine, rarity: "T", rating: 86, score: { pac: 75, sho: 70, pas: 77, dri: 79, def: 86, phy: 89 } },
+{ name: "Julian Draxler", team: "Paris Saint-Germain", image: fifty, rarity: "G", rating: 77, score: { pac: 72, sho: 75, pas: 77, dri: 80, def: 58, phy: 64 } },
+{ name: "Layvin Kurzawa", team: "Paris Saint-Germain", image: fiftyOne, rarity: "S", rating: 73, score: { pac: 66, sho: 65, pas: 72, dri: 75, def: 72, phy: 67 } },
+{ name: "Warren Zaire Emery", team: "Paris Saint-Germain", image: fiftyTwo, rarity: "S", rating: 74, score: { pac: 75, sho: 62, pas: 71, dri: 75, def: 69, phy: 71 } },
+{ name: "Cher Ndour", team: "Paris Saint-Germain", image: fiftyThree, rarity: "B", rating: 68, score: { pac: 55, sho: 61, pas: 68, dri: 68, def: 57, phy: 70 } },
+{ name: "Kylian Mbappe", team: "Paris Saint-Germain", image: fiftyFour, rarity: "T", rating: 92, score: { pac: 98, sho: 91, pas: 81, dri: 93, def: 37, phy: 79 } },
+{ name: "Ousmane Dembélé", team: "Paris Saint-Germain", image: fiftyFive, rarity: "T", rating: 89, score: { pac: 96, sho: 81, pas: 85, dri: 90, def: 40, phy: 60 } },
+{ name: "Marquinhos", team: "Paris Saint-Germain", image: fiftySix, rarity: "T", rating: 87, score: { pac: 78, sho: 56, pas: 75, dri: 74, def: 89, phy: 80 } },
+{ name: "Marco Asensio", team: "Paris Saint-Germain", image: fiftySeven, rarity: "G", rating: 83, score: { pac: 81, sho: 81, pas: 81, dri: 83, def: 43, phy: 63 } },
+{ name: "Fabian Ruiz", team: "Paris Saint-Germain", image: fiftyEight, rarity: "G", rating: 81, score: { pac: 55, sho: 78, pas: 79, dri: 80, def: 74, phy: 72 } },
+{ name: "Ángel Di María", team: "Paris Saint-Germain", image: fiftyNine, rarity: "I", rating: 88, score: { pac: 86, sho: 84, pas: 92, dri: 94, def: 53, phy: 73 } },
+{ name: "Gianluigi Donnarumma", team: "Paris Saint-Germain", image: sixty, rarity: "K", rating: 87, score: { div: 90, han: 82, kic: 79, ref: 89, spe: 52, pos: 83 } },
+{ name: "Aaron Ramsdale", team: "Arsenal", image: sixtyOne, rarity: "K", rating: 84, score: { div: 84, han: 79, kic: 87, ref: 86, spe: 48, pos: 81 } },
+{ name: "Bukayo Saka", team: "Arsenal", image: sixtyTwo, rarity: "T", rating: 88, score: { pac: 93, sho: 77, pas: 80, dri: 88, def: 45, phy: 62 } },
+{ name: "Gabriel Martinelli", team: "Arsenal", image: sixtyThree, rarity: "G", rating: 84, score: { pac: 89, sho: 78, pas: 76, dri: 86, def: 46, phy: 72 } },
+{ name: "Declan Rice", team: "Arsenal", image: sixtyFour, rarity: "T", rating: 87, score: { pac: 78, sho: 67, pas: 77, dri: 80, def: 87, phy: 85 } },
+{ name: "Fabio Vieira", team: "Arsenal", image: sixtyFive, rarity: "G", rating: 77, score: { pac: 74, sho: 71, pas: 77, dri: 77, def: 40, phy: 53 } },
+{ name: "Jakub Kiwior", team: "Arsenal", image: sixtySix, rarity: "S", rating: 74, score: { pac: 67, sho: 37, pas: 59, dri: 61, def: 75, phy: 77 } },
+{ name: "Cedric Soares", team: "Arsenal", image: sixtySeven, rarity: "S", rating: 73, score: { pac: 65, sho: 61, pas: 70, dri: 74, def: 73, phy: 62 } },
+{ name: "Mohamed Elneny", team: "Arsenal", image: sixtyEight, rarity: "S", rating: 75, score: { pac: 52, sho: 63, pas: 71, dri: 70, def: 72, phy: 70 } },
+{ name: "Jorginho", team: "Arsenal", image: sixtyNine, rarity: "G", rating: 83, score: { pac: 53, sho: 67, pas: 84, dri: 81, def: 74, phy: 73 } },
+{ name: "Thomas Partey", team: "Arsenal", image: seventy, rarity: "G", rating: 85, score: { pac: 68, sho: 71, pas: 82, dri: 80, def: 81, phy: 82 } },
+{ name: "Gabriel Jesus", team: "Arsenal", image: seventyOne, rarity: "T", rating: 86, score: { pac: 86, sho: 84, pas: 78, dri: 90, def: 42, phy: 80 } },
+{ name: "Thierry Henry", team: "Arsenal", image: seventyTwo, rarity: "I", rating: 92, score: { pac: 94, sho: 91, pas: 83, dri: 90, def: 53, phy: 80 } },
+{ name: "Raúl Albiol", team: "Villarreal FC", image: seventyThree, rarity: "G", rating: 81, score: { pac: 36, sho: 41, pas: 62, dri: 63, def: 85, phy: 68 } },
+{ name: "Ilias Akhomach", team: "Villarreal FC", image: seventyFour, rarity: "S", rating: 70, score: { pac: 71, sho: 66, pas: 66, dri: 72, def: 21, phy: 45 } },
+{ name: "Morales", team: "Villarreal FC", image: seventyFive, rarity: "G", rating: 79, score: { pac: 79, sho: 82, pas: 75, dri: 81, def: 52, phy: 67 } },
+{ name: "Yeremy Pino", team: "Villarreal FC", image: seventySix, rarity: "G", rating: 79, score: { pac: 76, sho: 73, pas: 76, dri: 79, def: 38, phy: 46 } },
+{ name: "Abraham", team: "Villarreal FC", image: seventySeven, rarity: "B", rating: 64, score: { pac: 58, sho: 40, pas: 54, dri: 48, def: 64, phy: 61 } },
+{ name: "Carlos Romero", team: "Villarreal FC", image: seventyEight, rarity: "B", rating: 65, score: { pac: 60, sho: 47, pas: 58, dri: 56, def: 63, phy: 60 } },
+{ name: "Parejo", team: "Villarreal FC", image: seventyNine, rarity: "T", rating: 86, score: { pac: 49, sho: 83, pas: 90, dri: 80, def: 73, phy: 69 } },
+{ name: "Denis Suárez", team: "Villarreal FC", image: eighty, rarity: "S", rating: 75, score: { pac: 63, sho: 71, pas: 78, dri: 78, def: 64, phy: 54 } },
+{ name: "Gerard Moreno", team: "Villarreal FC", image: eightyOne, rarity: "G", rating: 83, score: { pac: 77, sho: 85, pas: 77, dri: 81, def: 46, phy: 72 } },
+{ name: "Jorge Pascual", team: "Villarreal FC", image: eightyTwo, rarity: "S", rating: 72, score: { pac: 57, sho: 37, pas: 58, dri: 52, def: 73, phy: 56 } },
+{ name: "Forlan", team: "Villarreal FC", image: eightyThree, rarity: "I", rating: 89, score: { pac: 87, sho: 91, pas: 81, dri: 86, def: 56, phy: 78 } },
+{ name: "Filip Jörgensen", team: "Villarreal FC", image: eightyFour, rarity: "K", rating: 70, score: { div: 69, han: 73, kic: 72, ref: 71, spe: 36, pos: 70 } },
+]
+
+export default playerinfo;
